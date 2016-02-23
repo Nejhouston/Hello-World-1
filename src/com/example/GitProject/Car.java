bbbbd7d88a1b5de8e76ca426a1d73ecbdb4c46d7
Billy <Billy@BILLs-MacBook-Pro.local>
2016-02-22 19:22:34 -0800

    Added file

---
+++ src/com/example/GitProject/Car.java
@@ -0,0 +1,7 @@
+package com.example.GitProject;
+
+/**
+ * Created by Billy on 2/22/16.
+ */
+public class Car {
+}
---
+++ src/com/example/GitProject/Main.java
@@ -0,0 +1,11 @@
+package com.example.GitProject;
+
+/**
+ * Created by Billy on 2/22/16.
+ */
+public class Main {
+    public static void main(String[] args )
+    {
+
+    }
+}
